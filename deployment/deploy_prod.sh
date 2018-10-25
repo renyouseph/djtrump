72b630b43f123fc20cfeb8e4cc4eb01986050118
Reny Ouseph <renyouseph@gmail.com>
2018-10-25 22:04:40 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -5,6 +5,7 @@ ssh root@35.153.175.34 <<EOF
   git pull
   source /opt/envs/djtrump/bin/activate
   pip3 install -r requirements.txt
+  pip3 install http://initd.org/psycopg/upload/psycopg2-2.7.3.1.dev0/psycopg2-2.7.3.1.dev0-cp36-cp36m-manylinux1_x86_64.whl
   ./manage.py migrate
   sudo supervisorctl restart djtrump
   exit
