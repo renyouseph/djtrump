64c1b7cfd8a3bd90f7d4a8bf6f68a8b6c6229ed4
Reny Ouseph <renyouseph@gmail.com>
2018-10-25 21:50:07 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -4,7 +4,7 @@ ssh root@35.153.175.34 <<EOF
   cd djtrump
   git pull
   source /opt/envs/djtrump/bin/activate
-  pip install -r requirements.txt
+  pip3 install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djtrump
   exit
